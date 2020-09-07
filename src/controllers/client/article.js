b2c2591f60f0f31c92adbadcf7dbf66592c7dac1
mojie <sunjunjie@okay.cn>
2020-09-07 19:03:40 +0800

    feat: 完成页面基础渲染，使用mock数据搭建前端布局

--- src/controllers/client/article.js
+++ src/controllers/client/article.js
@@ -1,7 +1,21 @@
 async function articleDetail (ctx) {
   const title = '文章详情'
   await ctx.render('client/page/article', {
-    title
+    title,
+    detail: {
+      id: 432432423,
+      name: 'VSCode插件大全',
+      category: [{
+        id:1,
+        name: '源码分析'
+      }],
+      create_time: '1529462887135',
+      update_time: '1599475887135',
+      tag:[{
+        id:1,
+        name: '标签一',
+      }]
+    }
   })
 }
 export default {
--- src/controllers/client/home.js
+++ src/controllers/client/home.js
@@ -1,7 +1,20 @@
 async function indexPage (ctx) {
-  const title = 'home'
+  const title = '夜猫子前端日记'
+  const list = [1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => ({
+    id: i + '' + Math.floor(Math.random() * 99999999),
+    thumb: '',
+    name: 'VSCode插件大全' +i,
+    author: '摩诘',
+    category: [
+      {
+        id: 1,
+        name: '源码分析'
+      }
+    ]
+  }))
   await ctx.render('client/page/home', {
-    title
+    title,
+    latest: list
   })
 }
 export default {
